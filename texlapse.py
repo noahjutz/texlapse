dc27cac3123de95a77f0721a534ba6ac7348db62
Noah Jutz <noahjutz@tutanota.de>
2020-11-20 14:59:25 +0100

    Add top level functions

--- texlapse.py
+++ texlapse.py
@@ -7,5 +7,29 @@ def main():
     print("Hello world!")
 
 
+def clone_repo():
+    pass
+
+
+def get_commits():
+    pass
+
+
+def latexmk():
+    pass
+
+
+def pdf_to_images():
+    pass
+
+
+def combine_images():
+    pass
+
+
+def pngs_to_timelapse():
+    pass
+
+
 if __name__ == "__main__":
     main()
