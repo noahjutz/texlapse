4eee58b954bc885af012eeaa17551f18899cea66
Noah Jutz <noahjutz@tutanota.de>
2020-11-20 18:22:24 +0100

    Add run(), clone_repo() impl

--- texlapse.py
+++ texlapse.py
@@ -16,8 +16,19 @@ def main():
     pngs_to_timelapse()
 
 
+def run(*args):
+    for command in args:
+        print(f"==> {command}")
+        output = subprocess.run([f"{command} &> /dev/null"], shell=True, )
+        is_success = output.returncode == 0
+        print(f"    {'SUCCESS' if is_success else 'FAIL'}")
+
+
 def clone_repo():
-    pass
+    run(
+        "mkdir input",
+        "git -C input/ clone https://github.com/noahjutz/w-seminararbeit.git"
+    )
 
 
 def get_commits():
